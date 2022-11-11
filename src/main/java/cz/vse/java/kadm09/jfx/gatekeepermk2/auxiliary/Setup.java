c0d65ef5bc901435ea78a510f5ab1e5502e85f92
Martin Kadlec2 <martin.kadlec2@lundegaard.eu>
2022-11-11 16:22:27 +0100

    Instantiated map

--- src/main/java/cz/vse/java/kadm09/jfx/gatekeepermk2/auxiliary/Setup.java
+++ src/main/java/cz/vse/java/kadm09/jfx/gatekeepermk2/auxiliary/Setup.java
@@ -7,9 +7,10 @@ import cz.vse.java.kadm09.jfx.gatekeepermk2.knight.TheKnight;
 public class Setup {
 
     public static void initGame () {
-        Map.fillMap();
+        Map gameMap = new Map();
+        gameMap.fillMap();
         TheKnight player = new TheKnight(200,50,2,15,250,new Coordinates(4,2),false);
-        Map.printPosition(player);
+        //System.out.println(gameMap.printPosition(player));
 
     }
 

2e5a0acbce23ea381536601c005a39a4d9e3d3c6
Martin Kadlec2 <martin.kadlec2@lundegaard.eu>
2022-11-15 18:17:15 +0100

    Fixed previous commit issues with ENUM addition

--- src/main/java/cz/vse/java/kadm09/jfx/gatekeepermk2/gameLogic/GameState.java
+++ src/main/java/cz/vse/java/kadm09/jfx/gatekeepermk2/gameLogic/GameState.java
@@ -13,13 +13,27 @@ public enum GameState {
      * @see Commands
      */
     EXPLORATION,
-
     /**
      * Game expects directional input for the move command
      * @see cz.vse.java.kadm09.jfx.gatekeepermk2.knight.TheKnight#moveKnight(String, TheKnight, Map)
      */
     MOVEMENT,
-    COMBAT
+    COMBAT,
+    /**
+     * Game is expecting the name of the item to be used as an input.
+     */
+    INVENTORY, //TODO: , potentially problematic during combat
+    /**
+     * Player is shoping for armor upgrades
+     */
+    ARMORSMITH,
+    /**
+     * Player is shopping for weapon upgrades
+     */
+    HUNTSMAN,
+    SPELLCAST, //todo: potentially problematic in relation to combat, might need to rework string processing logic for this command
+    ENDGAME
+
 
 
 

6f6bc830a51dd160714271a2b260fdea6a7b8e97
Martin Kadlec2 <martin.kadlec2@lundegaard.eu>
2022-12-06 18:53:30 +0100

    JUNIT - Rebuild the standard Maven/JUNIT integration schema and introduce TheKnightTest

---
+++ src/test/java/cz/vse/java/kadm09/jfx/gatekeepermk2/knight/TheKnightTest.java
@@ -0,0 +1,90 @@
+package cz.vse.java.kadm09.jfx.gatekeepermk2.knight;
+
+import cz.vse.java.kadm09.jfx.gatekeepermk2.gameLogic.Game;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TheKnightTest {
+
+    Game TestingGrounds = new Game();
+
+    @BeforeEach
+    void resetGame () {
+        TestingGrounds = new Game();
+    }
+
+    @Test
+    void checkCorrectBaseSetting () {
+        assertAll(("Check if base stats are correctly set on game load up"),
+                () -> assertEquals(225, TestingGrounds.getPlayer().getCurrentHealth()),
+                () -> assertEquals(75, TestingGrounds.getPlayer().getCurrentMana()),
+                () -> assertEquals(4, TestingGrounds.getPlayer().getArmor()),
+                () -> assertEquals(30, TestingGrounds.getPlayer().getDamage()),
+                () -> assertEquals(500, TestingGrounds.getPlayer().getGoldHeld())
+        );
+    }
+
+
+    @Test
+    void checkRudimentaryMoves(){
+        assertEquals(4,TestingGrounds.getPlayer().getPosition().getHorizontal());
+        assertEquals(2,TestingGrounds.getPlayer().getPosition().getVertical());
+
+        TestingGrounds.getPlayer().moveKnight("north",TestingGrounds);
+        TestingGrounds.getPlayer().moveKnight("north",TestingGrounds);
+
+        assertEquals(2,TestingGrounds.getPlayer().getPosition().getHorizontal());
+        assertEquals(2,TestingGrounds.getPlayer().getPosition().getVertical());
+
+        TestingGrounds.getPlayer().moveKnight("west",TestingGrounds);
+        TestingGrounds.getPlayer().moveKnight("east",TestingGrounds);
+
+        assertEquals(2,TestingGrounds.getPlayer().getPosition().getHorizontal());
+        assertEquals(2,TestingGrounds.getPlayer().getPosition().getVertical());
+
+    }
+
+    @Test
+    void checkOutOfBoundsExceptionHandling () {
+        assertEquals(4,TestingGrounds.getPlayer().getPosition().getHorizontal());
+        assertEquals(2,TestingGrounds.getPlayer().getPosition().getVertical());
+
+        TestingGrounds.getPlayer().moveKnight("south",TestingGrounds);
+
+        assertEquals(4,TestingGrounds.getPlayer().getPosition().getHorizontal());
+        assertEquals(2,TestingGrounds.getPlayer().getPosition().getVertical());
+
+        TestingGrounds.getPlayer().moveKnight("west",TestingGrounds);
+        TestingGrounds.getPlayer().moveKnight("west",TestingGrounds);
+        TestingGrounds.getPlayer().moveKnight("west",TestingGrounds);
+        TestingGrounds.getPlayer().moveKnight("west",TestingGrounds);
+
+
+        assertEquals(4,TestingGrounds.getPlayer().getPosition().getHorizontal());
+        assertEquals(0,TestingGrounds.getPlayer().getPosition().getVertical());
+
+
+        TestingGrounds.getPlayer().moveKnight("east",TestingGrounds);
+        TestingGrounds.getPlayer().moveKnight("east",TestingGrounds);
+        TestingGrounds.getPlayer().moveKnight("east",TestingGrounds);
+        TestingGrounds.getPlayer().moveKnight("east",TestingGrounds);
+
+        assertEquals(4,TestingGrounds.getPlayer().getPosition().getHorizontal());
+        assertEquals(4,TestingGrounds.getPlayer().getPosition().getVertical());
+
+        TestingGrounds.getPlayer().moveKnight("north",TestingGrounds);
+        TestingGrounds.getPlayer().moveKnight("north",TestingGrounds);
+        TestingGrounds.getPlayer().moveKnight("north",TestingGrounds);
+        TestingGrounds.getPlayer().moveKnight("north",TestingGrounds);
+        TestingGrounds.getPlayer().moveKnight("north",TestingGrounds);
+        TestingGrounds.getPlayer().moveKnight("north",TestingGrounds);
+
+        assertEquals(0,TestingGrounds.getPlayer().getPosition().getHorizontal());
+        assertEquals(4,TestingGrounds.getPlayer().getPosition().getVertical());
+
+    }
+
+}
